1235099d88087ef2a31fc5205fd9ef39e08c123a
salih1f1 <salih1f1@hotmail.com>
2015-11-03 18:20:54 +0200

    Breakout class added

---
+++ Game/src/Breakout.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.EventQueue;
+
+public class Breakout extends JFrame {
+    public Breakout(){
+        initUI();
+    }
+
+    private void initUI(){
+        add(new Board());
+        setTitle("Breakout");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(Commons.WIDTH, Commons.HEIGTH);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Breakout game = new Breakout();
+                game.setVisible(true);
+            }
+        });
+    }
+}
